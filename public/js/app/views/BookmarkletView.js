68fb72fb8c27c327f98e0c30c1d870d9bb5ba709
Alex Olshansky <i@creagenics.com>
2016-03-09 12:21:52 +0100

    [bookmarklet] Insert Instagram thumbnail automatically

--- public/js/app/views/BookmarkletView.js
+++ public/js/app/views/BookmarkletView.js
@@ -18,6 +18,14 @@ define(["app/app",
       })
     },
 
+    selectInstagramThumbnail: function() {
+      var re = /https?:\/\/www\.instagram\.com\/p\/([\w-]+)\//i;
+      var m = document.referrer.match(re)
+      if (m && m[1]) {
+        location.hash = 'https://www.instagram.com/p/' + m[1] + '/media/?size=l'
+      }
+    }.on('didInsertElement'),
+
     selectYoutubeThumbnail: function() {
       var re = /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*[^\w\s-])([\w-]{11})(?=[^\w-]|$)[?=&+%\w.-]*/i;
       var m = document.referrer.match(re)
